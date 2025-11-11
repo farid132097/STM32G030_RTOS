b3a19116848718762bcad980186d91972b9e6ce9
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2025-11-11 17:06:46 +0600

    Added: system variables init in macro

--- kernel.h
+++ kernel.h
@@ -9,14 +9,19 @@
 	
 */
 
-#ifndef _KERNEL_H_
-#define _KERNEL_H_
+#ifndef  _KERNEL_H_
+#define  _KERNEL_H_
 
-void    Kernel_Init(void);
-void    Kernel_Start_Tasks(void);
-void    Kernel_Task_Create(void (*func)(void), unsigned char priority);
-void    Kernel_Idle_Task(void);
-void    Kernel_Task_Sleep(unsigned int val);
+#include "stdint.h"
+
+void     Kernel_Init(void);
+void     Kernel_Start_Tasks(void);
+void     Kernel_Task_Create(void (*func)(void), unsigned char priority);
+void     Kernel_Idle_Task(void);
+void     Kernel_Task_Sleep(unsigned int val);
+uint32_t Kernel_Tick_SS32_Get(void);
+uint64_t Kernel_Tick_SS64_Get(void);
+uint32_t Kernel_Tick_S_Get(void);
 
 #endif
 
