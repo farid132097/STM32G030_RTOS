3ce4bb075299ad318b1dc798dafe3695663cfce4
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2026-05-07 11:02:54 +0600

    Added: timeout functions inside kernel

--- kernel.h
+++ kernel.h
@@ -6,7 +6,7 @@
 	Source        : https://github.com/farid132097/STM32G030_RTOS
   Description   : ARM Cortex M0+ kernel for bare-metal RTOS
   Created       : Sep 02, 2025, 09:30 PM
-  Last Modified : May 06, 2026, 02:29 PM
+  Last Modified : May 07, 2026, 11:01 AM
 	
 */
 
@@ -17,12 +17,14 @@
 
 void     Kernel_Init(void);
 void     Kernel_Start_Tasks(void);
-void     Kernel_Task_Create(void (*func)(void), unsigned char priority);
+void     Kernel_Task_Create(void (*func)(void), uint32_t priority);
 void     Kernel_Idle_Task(void);
-void     Kernel_Task_Sleep(unsigned int val);
+void     Kernel_Task_Sleep(uint32_t val);
 uint32_t Kernel_Tick_SS32_Get(void);
 uint64_t Kernel_Tick_SS64_Get(void);
 uint32_t Kernel_Tick_S_Get(void);
+void     Kernel_Timeout_Set(uint32_t val);
+uint32_t Kernel_Is_Timeout(void);
 uint32_t Kernel_CPU_Usage_Get(void);
 
 #endif
