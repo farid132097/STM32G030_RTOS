f4cfe53ba1c566f2eab592e40989d74ed009fc45
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2026-05-07 18:04:08 +0600

    Added: device debug config inside kernel.s

--- Objects/system_stm32g0xx.d
+++ Objects/system_stm32g0xx.d
@@ -1,4 +1,4 @@
 ./objects/system_stm32g0xx.o: RTE\Device\system_stm32g0xx.c \
   RTE\Device\stm32g0xx.h RTE\Device\stm32g030xx.h \
-  C:\Keil\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm0plus.h \
+  C:\Keil\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm0plus.h \
   RTE\Device\system_stm32g0xx.h
--- kernel.h
+++ kernel.h
@@ -1,13 +1,12 @@
 
-/*
 
+/*
    File          : kernel.h
    Author        : MD. Faridul Islam (faridmdislam@gmail.com)
 	 Source        : https://github.com/farid132097/STM32G030_RTOS
    Description   : ARM Cortex M0+ kernel for bare-metal RTOS
    Created       : Sep 02, 2025, 09:30 PM
-   Last Modified : May 07, 2026, 02:48 PM
-	
+   Last Modified : May 07, 2026, 05:33 PM
 */
 
 
