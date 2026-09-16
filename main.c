b83ef2509d1cd1b4dae730994234d7c3280d784a
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2026-09-16 17:42:03 +0600

    Modified: ntask and stack size in config

--- main.c
+++ main.c
@@ -7,9 +7,9 @@
 int main(void){
 	
 	Kernel_Init();
-	Kernel_Task_Create(Tasks_Task1, 3);
+	Kernel_Task_Create(Tasks_Task1, 2);
 	Kernel_Task_Create(Tasks_Task2, 0);
-	Kernel_Task_Create(Tasks_Task3, 2);
+	Kernel_Task_Create(Tasks_Task3, 1);
 	Kernel_Start_Tasks();
 	
 	while(1){
